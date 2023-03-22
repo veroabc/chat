b97e2980528a68cd5fdcb54428a4733de504d707
Roni <91472809+veroabc@users.noreply.github.com>
2023-03-22 12:10:21 +0100

    new

---
+++ chat.py
@@ -0,0 +1,49 @@
+from llama_index import SimpleDirectoryReader, GPTListIndex, readers, GPTSimpleVectorIndex, LLMPredictor, PromptHelper
+from langchain import OpenAI
+import sys
+import os
+from IPython.display import Markdown, display
+
+def construct_index(directory_path):
+    # set maximum input size
+    max_input_size = 4096
+    # set number of output tokens
+    num_outputs = 2000
+    # set maximum chunk overlap
+    max_chunk_overlap = 20
+    # set chunk size limit
+    chunk_size_limit = 600 
+
+    # define LLM
+    llm_predictor = LLMPredictor(llm=OpenAI(temperature=0.5, model_name="text-davinci-003", max_tokens=num_outputs))
+    prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
+ 
+    documents = SimpleDirectoryReader(directory_path).load_data()
+    
+    index = GPTSimpleVectorIndex(
+        documents, llm_predictor=llm_predictor, prompt_helper=prompt_helper
+    )
+
+    index.save_to_disk('index.json')
+
+    return index
+
+def ask_ai():
+    index = GPTSimpleVectorIndex.load_from_disk('index.json')
+    is_running = True
+    while is_running: 
+        query = input("What do you want to ask? ")        
+        bot = index.query(query, response_mode="compact")
+        print(f'{bot.response}\n\nWas this answer helpful? If yes, please enter "yes" else "no"')
+
+        is_helpful_answer = "yes" in input("Was this answer helpful?: ").lower()
+        
+        if is_helpful_answer:
+            is_running = False
+        
+        
+os.environ["OPENAI_API_KEY"] = "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"
+
+construct_index("context_data/data")
+
+ask_ai()
